da3b0d1c0b400b257d47c52ad738464a119a149a
travisf <travisf@jfrog.com>
2016-06-23 10:56:12 -0700

    Fixed SSL exception
    Some versions of SSL have a bug that causes an exception to be thrown
    when a connection is made using TLSv1.1/TLSv1.2. This hack works around
    this problem by forcing TLSv1.0.

--- NexusToArtifactory.py
+++ NexusToArtifactory.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python2
 import os
+import ssl
 import unicurses
+from functools import wraps
 from nex2art.menu import Safety, Main
 from nex2art.core import Screen
+
+# Some versions of SSL have a bug that causes an exception to be thrown when
+# a connection is made using TLSv1.1/TLSv1.2. This hack works around this
+# problem by forcing TLSv1.0.
+def fixssl():
+    def sslwrap(func):
+        @wraps(func)
+        def bar(*args, **kw):
+            kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+            return func(*args, **kw)
+        return bar
+    ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
 # Start the tool by initializing unicurses and creating a new Screen object.
 # Before doing so, set the ESCDELAY environment variable to 25, in an attempt to
@@ -10,6 +24,7 @@ from nex2art.core import Screen
 # editing.
 if __name__ == '__main__':
     os.environ.setdefault('ESCDELAY', '25')
+    fixssl()
     try:
         stdscr = unicurses.initscr()
         unicurses.noecho()
