d38e8c51f2b806bc394bd7640e1634e892cb424f
travisf <travisf@jfrog.com>
2018-04-17 14:06:26 -0700

    UI optimization
    The ChooseList UI module now builds its options list lazily (when
    displayed, rather than when created). This prevents huge ChooseLists
    from slowing the UI down. Tested with 1000 users and 1000 groups,
    opening the user list menu takes roughly two seconds with this update,
    and over seventeen seconds without it.

--- nex2art/menu/ChooseList.py
+++ nex2art/menu/ChooseList.py
@@ -4,14 +4,21 @@ class ChooseList(Menu):
     def __init__(self, scr, path, typ, tform, opts):
         self.leaf = True
         Menu.__init__(self, scr, path, "Choose " + typ)
-        self.pagedopts = []
-        for opt in opts:
-            popt = self.mkopt(None, tform(opt), [self.setchoice(opt), None])
-            self.pagedopts.append(popt)
-        self.pagedopts.sort(key=lambda x: x['text'])
+        self.tform = tform
+        self.chooseopts = opts
+        self.pagedopts = None
         self.opts = [
             None,
             self.mkopt('q', "Back", None, hdoc=False)]
+
+    def initialize(self):
+        if self.pagedopts != None: return
+        self.pagedopts = []
+        for opt in self.chooseopts:
+            choice = [self.setchoice(opt), None]
+            popt = self.mkopt(None, self.tform(opt), choice)
+            self.pagedopts.append(popt)
+        self.pagedopts.sort(key=lambda x: x['text'])
 
     def setchoice(self, opt):
         def f(_): self.option['val'] = opt
